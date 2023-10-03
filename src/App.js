62a4a35d4c53f4f0fb7857db6a94c643b4537c8e
ManuBhardwaj001 <100307788+ManuBhardwaj001@users.noreply.github.com>
2023-10-03 23:16:40 +0530

    added gsc file & react router v6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import HomeScreen from "../src/Homescreen"
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
-    </div>
+      {/* <HomeScreen /> */}
+      <Router>
+        <div>
+
+          <Routes>
+            <Route path="/about" element={<div>
+              <h1>About Page</h1>
+              <p>This is the about page content.</p>
+            </div>} >
+            </Route>
+            <Route path="/" element={<HomeScreen />}>
+            </Route>
+          </Routes>
+        </div>
+      </Router >
+    </div >
   );
 }
+
 
 export default App;
