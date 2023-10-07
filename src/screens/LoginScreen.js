6d5f22a5b188ab64a7e014e9591033f7b5cb1802
ManuBhardwaj001 <100307788+ManuBhardwaj001@users.noreply.github.com>
2023-10-07 23:05:56 +0530

    updated loginScreen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -2,9 +2,15 @@ import React from "react";
 import "./LoginScreen.css"
 
 function LoginScreen() {
-    return (<div className="loginScreen">
+    return (
+        <div className="loginScreen"> 
+            <div className="loginScreen__background">
+                <img className="loginScreen__logo"
+                    src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" />
 
-    </div>
+                <button className="loginScreen__button">Sign In</button>
+            </div>
+        </div>
 
     );
 }
