05a48d948323ecd5ce132bc906a1fb936de724ca
ManuBhardwaj001 <100307788+ManuBhardwaj001@users.noreply.github.com>
2023-10-18 15:55:13 +0530

    signUp & signIn functionality setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HomeScreen from "./screens/Homescreen"
 import './App.css';
+import { auth } from './firebase';
 import LoginScreen from "./screens/LoginScreen"
 import {
   BrowserRouter as Router,
@@ -12,24 +13,41 @@ function App() {
 
   const user = null
 
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        console.log(userAuth);
+        //Logged in
+      }
+      else {
+        //Logged out
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    }
+
+  }, [])
+
   return (
     <div className="app">
       {/* <HomeScreen /> */}
       <Router>
         <div>
           {!user ? (
-            <LoginScreen/>
-          ) :(
+            <LoginScreen />
+          ) : (
 
-          <Routes>
-            <Route path="/" 
-            element={<HomeScreen />}>
-            </Route>
-            {/* <Route path="/about" element={<div>
+            <Routes>
+              <Route path="/"
+                element={<HomeScreen />}>
+              </Route>
+              {/* <Route path="/about" element={<div>
               <h1>TV Shows</h1>
             </div>} >
             </Route> */}
-          </Routes>
+            </Routes>
           )}
         </div>
       </Router >
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app";
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyBwNoV1lA8Dp6GTrQqRj_na9DM_vRMvXD0",
     authDomain: "netflix-clone-50e6d.firebaseapp.com",
@@ -7,3 +11,10 @@ const firebaseConfig = {
     appId: "1:933175273590:web:908ea17792efb9361e1757",
     measurementId: "G-Y3EDDWNB3P"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
