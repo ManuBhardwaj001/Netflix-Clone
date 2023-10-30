03250292a67e6a0fff6249b1f21638d720e9ae5d
ManuBhardwaj001 <100307788+ManuBhardwaj001@users.noreply.github.com>
2023-10-30 23:45:30 +0530

    minor UI improvements SignupScreen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -34,10 +34,15 @@ function SignupScreen() {
     return <div className="signupScreen">
         <form >
             <h1>Sign In</h1>
-            <input ref={emailRef} placeholder="Email" type="email" />
+            <input ref={emailRef} placeholder="Email or phone number" type="email" />
             <input ref={passwordRef} placeholder="Password" type="password" />
             <button onClick={signIn} type="submit">Sign In</button>
-
+            <div className="remember__needHelp">
+                <div className="remember">
+                    <input className="checkbox__remember" type="checkbox" value="true" /><span className="label__remember">Remember me</span>
+                </div>
+                <span className="needHelp">Need help?</span>
+            </div>
             <div className="singupScreen__register">
                 <span className="signupScreen__gray">New to Netflix? </span><span onClick={register} className="signupScreen__link">Sign Up now</span>
             </div>
