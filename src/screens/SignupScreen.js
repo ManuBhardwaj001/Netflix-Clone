86ff80d48f40697cca91685f8d2dc8472255dc2b
ManuBhardwaj001 <100307788+ManuBhardwaj001@users.noreply.github.com>
2023-10-31 23:53:40 +0530

    signupScreen bottom-section UI update

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -37,14 +37,31 @@ function SignupScreen() {
             <input ref={emailRef} placeholder="Email or phone number" type="email" />
             <input ref={passwordRef} placeholder="Password" type="password" />
             <button onClick={signIn} type="submit">Sign In</button>
-            <div className="remember__needHelp">
-                <div className="remember">
-                    <input className="checkbox__remember" type="checkbox" value="true" /><span className="label__remember">Remember me</span>
+            <div className="signupScreen__bottom">
+                <div className="remember__needHelp">
+                    <div className="remember">
+                        <input className="checkbox__remember" id="rememberMe" type="checkbox" value="true" />
+                        <label for="rememberMe">
+                            <span className="label__remember">Remember me</span>
+                        </label>
+                    </div>
+                    <span className="needHelp">Need help?</span>
                 </div>
-                <span className="needHelp">Need help?</span>
-            </div>
-            <div className="singupScreen__register">
-                <span className="signupScreen__gray">New to Netflix? </span><span onClick={register} className="signupScreen__link">Sign Up now</span>
+                <div className="singupScreen__register">
+                    <span className="signupScreen__gray">New to Netflix? </span><span onClick={register} className="signupScreen__link">Sign Up now</span>
+                </div>
+                <div className="signupScreen__recaptcha">
+                    <p className="recaptcha">
+                        <span>This page is protected by Google reCAPTCHA to ensure you're not a bot.</span>
+                        &nbsp;
+                        <button className="captcha__learnMore">
+                            Learn more.
+                        </button>
+                    </p>
+                    <div className="recaptcha__disclosure">
+                        <span>The information collected by Google reCAPTCHA is subject to the Google Privacy Policy and Terms of Service, and is used for providing, maintaining, and improving the reCAPTCHA service and for general security purposes (it is not used for personalised advertising by Google).</span>
+                    </div>
+                </div>
             </div>
         </form>
     </div>;
